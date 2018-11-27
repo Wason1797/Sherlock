3412d70e0a18e27d5e5d49063ab1982a21e2acfc
Christopher Rojas <csrm1997@hotmail.com>
2018-11-26 21:04:37 -0500

    GPS and some visual changes
    GPS is working on real time, added sherlock icon, changed the gps viewer and changed background and text colors

--- SherlockPage/js/xample.js
+++ SherlockPage/js/xample.js
@@ -1,16 +1,17 @@
+var map, infoWindow, gps, firebase;
+
+//Connection var to firebase
+var config = {
+    apiKey: "<API_KEY>",
+    authDomain: "<PROJECT_ID>.firebaseapp.com",
+    databaseURL: "https://<DATABASE_NAME>.firebaseio.com",
+    projectId: "<PROJECT_ID>",
+    storageBucket: "<BUCKET>.appspot.com",
+    messagingSenderId: "<SENDER_ID>",
+};
+
 function charge() {
-    //Change config var with config that firebase provides you
-    var config = {
-        apiKey: "<API_KEY>",
-        authDomain: "<PROJECT_ID>.firebaseapp.com",
-        databaseURL: "https://<DATABASE_NAME>.firebaseio.com",
-        projectId: "<PROJECT_ID>",
-        storageBucket: "<BUCKET>.appspot.com",
-        messagingSenderId: "<SENDER_ID>",
-      };
-      firebase.initializeApp(config);
     log();
-    gps();
 }
 // Save a change in database as log
 // Select all your database field you need to watch as log in your app
@@ -26,6 +27,26 @@ function gps() {
     //Only change database/gps_field with your API URI to get GPS
     var starCountRef = firebase.database().ref('database/gps_field');
     starCountRef.on('value', function (snapshot) {
-        document.getElementById("gps").innerHTML = snapshot.val();
+        gps = snapshot.val().split(';');
+        var botPosition = new google.maps.LatLng(gps[0], gps[1]);
+        //Creates the map into the div map centering into the bot position and zoom of 20
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: botPosition,
+            zoom: 20
+        });
+        //Creates the red market locator for gps Change project name
+        marker = new google.maps.Marker({
+            position: botPosition,
+            title: "Project name"
+        });
+        marker.setMap(map);
     });
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+        'Error: The Geolocation service failed.' :
+        'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
 }
